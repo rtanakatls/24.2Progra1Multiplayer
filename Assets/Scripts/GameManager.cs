b1ced60c7e70853e5cc58803ca843a3b422e03e8
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-11-08 22:52:36 -0500

    Added multiplayer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject playerPrefab;
+
+    void Start()
+    {
+        if(PhotonNetwork.IsConnectedAndReady && Player.LocalInstance == null)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 10, 0), Quaternion.identity);
+        }
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        if (Player.LocalInstance == null)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 10, 0), Quaternion.identity);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviourPun
+{
+    private static GameObject localInstance;
+
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+
+    public static GameObject LocalInstance { get { return localInstance; } }
+    
+
+    private void Awake()
+    {
+        if(photonView.IsMine)
+        {
+            localInstance = gameObject;
+        }
+        DontDestroyOnLoad(gameObject);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!photonView.IsMine || !PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
+    }
+}
